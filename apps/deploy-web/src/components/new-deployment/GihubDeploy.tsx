490baffcbeeabb7c39f6a740eda4ab40051fc2a4
dharamveergit <dharamveersingh.em@gmail.com>
2024-07-04 00:08:51 +0530

    feat: Add lazy loading for images in ManifestEdit component

--- apps/deploy-web/src/components/new-deployment/GihubDeploy.tsx
+++ apps/deploy-web/src/components/new-deployment/GihubDeploy.tsx
@@ -1,9 +1,25 @@
 import { useEffect, useState } from "react";
 import { useMutation, useQuery } from "react-query";
-import { Button, Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue, Spinner } from "@akashnetwork/ui/components";
-import { Skeleton } from "@mui/material";
-import axios from "axios";
-import { Bitbucket, Github, GitlabFull } from "iconoir-react";
+import {
+  Button,
+  Card,
+  CardContent,
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+  Input,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Separator,
+  Spinner
+} from "@akashnetwork/ui/components";
+import { cn } from "@akashnetwork/ui/utils";
+import axios, { AxiosError } from "axios";
+import { Bitbucket, Github, GithubCircle, GitlabFull, Lock, NavArrowDown } from "iconoir-react";
 import { nanoid } from "nanoid";
 
 import { Service } from "@src/types";
@@ -26,8 +42,21 @@ const GithubDeploy = ({ setValue, services }: { setValue: any; services: Service
       });
       return response.data;
     },
+    onError: (error: AxiosError<{ message: string }>) => {
+      if (error?.response?.data?.message === "Bad credentials") {
+        console.log("Bad credentials");
+        localStorage.removeItem("token");
+        setToken(null);
+        handleLogin();
+        return;
+      }
+    },
     onSettled: data => {
+      console.log(data);
+
       if (data?.message === "Bad credentials") {
+        console.log("Bad credentials");
+
         localStorage.removeItem("token");
         setToken(null);
         return;
@@ -66,62 +95,143 @@ const GithubDeploy = ({ setValue, services }: { setValue: any; services: Service
   return (
     <>
       <div className="mt-6 flex flex-col gap-5 rounded border bg-card px-6 py-6 text-card-foreground">
-        <h1 className="font-semibold">Source Code</h1>
+        <h1 className="font-semibold">Configure</h1>
+        <div className="flex flex-col gap-5 rounded border bg-card px-6 py-6 text-card-foreground">
+          <h1 className="font-semibold">Source Code</h1>
 
-        <div className="flex flex-col items-center justify-center gap-6 rounded-sm border py-8">
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-lg font-bold text-primary">Connect Account</h1>
-            <p className="text-sm text-muted-foreground">Connect your GitHub account to use the GitHub integration.</p>
-          </div>
-          <div className="flex gap-3">
-            <Button variant="outline" className="">
-              <Bitbucket className="mr-2" />
-              Bitbucket
-            </Button>
-            <Button variant="outline" className="">
-              <GitlabFull className="mr-2" />
-              GitLab
-            </Button>
-            <Button onClick={handleLogin} variant="outline" className="">
-              <Github className="mr-2" />
-              Github
-            </Button>
-          </div>
-        </div>
-      </div>
-      {isLoading && (
-        <div className="mt-6 flex flex-col gap-5 rounded border bg-card px-6 py-6 text-card-foreground">
-          <h1 className="font-semibold">Repos</h1>
-          <div className="flex flex-col items-center justify-center gap-6 rounded-sm border px-5 py-8">
-            <Skeleton variant="rectangular" width="100%" height={50} className="rounded" />
-            <Skeleton variant="rectangular" width="100%" height={50} className="rounded" />
-            <Skeleton variant="rectangular" width="100%" height={50} className="rounded" />
+          <div className="flex flex-col items-center justify-center gap-6 rounded-sm border py-8">
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-lg font-bold text-primary">Connect Account</h1>
+              <p className="text-sm text-muted-foreground">Connect your GitHub account to use the GitHub integration.</p>
+            </div>
+            <div className="flex gap-3">
+              <Button variant="outline" className="">
+                <Bitbucket className="mr-2" />
+                Bitbucket
+              </Button>
+              <Button variant="outline" className="">
+                <GitlabFull className="mr-2" />
+                GitLab
+              </Button>
+              <Button onClick={handleLogin} variant="outline" className="">
+                <Github className="mr-2" />
+                Github
+              </Button>
+            </div>
           </div>
         </div>
-      )}
-      {repos?.length > 0 && (
-        <div className="mt-6 flex flex-col gap-5 rounded border bg-card px-6 py-6 text-card-foreground">
-          <h1 className="font-semibold">Repos</h1>
-
-          <div className="flex flex-col items-center justify-center divide-y rounded-sm border">
-            {repos.map((repo: any) => (
-              <Repo key={repo.id} repo={repo} services={services} setValue={setValue} token={token} />
-            ))}
-          </div>
+        <div className="grid grid-cols-2 gap-6">
+          <Repos repos={repos} setValue={setValue} token={token} isLoading={isLoading} />
+          <Branches repos={repos} services={services} setValue={setValue} token={token} />
         </div>
-      )}
+      </div>{" "}
+      <Details services={services} setValue={setValue} />
     </>
   );
 };
 
 export default GithubDeploy;
 
-const Repo = ({ repo, services, setValue, token }) => {
-  const selected = services.find(s => s?.env?.find(e => e.key === "REPO_URL" && e.value === repo.html_url));
+const Details = ({ services, setValue }) => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <Collapsible
+      open={expanded}
+      onOpenChange={value => {
+        setExpanded(value);
+      }}
+    >
+      <Card className="mt-4 rounded-sm border border-muted-foreground/20">
+        <CardContent className="p-0">
+          <CollapsibleTrigger asChild>
+            <div className="flex items-center justify-between p-4">
+              <h1 className="font-semibold">Details</h1>
+              <NavArrowDown fontSize="1rem" className={cn("transition-all duration-100", { ["rotate-180"]: expanded })} />
+            </div>
+          </CollapsibleTrigger>
+          {expanded && <Separator />}
+          <CollapsibleContent>
+            <div className="grid grid-cols-2 gap-6 p-5">
+              <CustomInput label="Root Directory" description="A unique name for your web service." placeholder="eg. src" />
+              <CustomInput label="Build Directory" description="The Repository Branch used for your private service" placeholder="eg. anything" />
+              <CustomInput label="Build Command" description="A unique name for your web service." placeholder="$ yarn" />
+              <CustomInput label="Start Command" description="The Repository Branch used for your private service" placeholder="$ yarn start" />
+            </div>
+          </CollapsibleContent>
+        </CardContent>
+      </Card>
+    </Collapsible>
+  );
+};
+
+const CustomInput = ({ label, description, placeholder }: { label: string; description: string; placeholder: string }) => {
+  return (
+    <div className="flex flex-col gap-5 rounded border bg-card px-6 py-6 text-card-foreground">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-semibold">{label}</h1>
+        <p className="text-muted-foreground">{description}</p>
+      </div>
+      <Input placeholder={placeholder} />
+    </div>
+  );
+};
+
+const Repos = ({ repos, setValue, token, isLoading }) => {
+  const [open, setOpen] = useState(false);
+  console.log(repos);
+
+  return (
+    <div className="flex flex-col gap-5 rounded border bg-card px-6 py-6 text-card-foreground">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-semibold">Select Repository</h1>
+        <p className="text-muted-foreground">The Repository Branch used for your private service</p>
+      </div>
+
+      <Select
+        onOpenChange={value => {
+          setOpen(value);
+        }}
+        open={open}
+        onValueChange={value => {
+          setValue("services.0.env", [
+            { id: nanoid(), key: "REPO_URL", value: value, isSecret: false },
+            { id: nanoid(), key: "BRANCH_NAME", value: "main", isSecret: false },
+            { id: nanoid(), key: "ACCESS_TOKEN", value: token, isSecret: true }
+          ]);
+        }}
+      >
+        <SelectTrigger className="w-full">
+          <div className="flex items-center gap-2">
+            {isLoading && <Spinner size="small" />}
+            <SelectValue placeholder={"Select Repository"} />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {repos?.map((repo: any) => (
+              <SelectItem key={repo.html_url} value={repo.html_url}>
+                <div className="flex items-center">
+                  <GithubCircle className="mr-2" />
+                  {repo.name}
+                  {/* if is private add a lock icon */}
+                  {repo.private && <Lock className="ml-1 text-xs" />}
+                </div>
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+const Branches = ({ repos, services, setValue, token }) => {
+  const repo = repos?.find(r => r.html_url === services[0]?.env?.find(e => e.key === "REPO_URL")?.value);
+  const selected = services?.find(s => s?.env?.find(e => e.key === "REPO_URL" && e.value === repo.html_url));
   console.log(selected);
   const [packageJson, setPackageJson] = useState<any>(null);
   const { data: branches, isLoading: branchesLoading } = useQuery({
-    queryKey: ["branches", repo.full_name],
+    queryKey: ["branches", repo?.full_name],
     queryFn: async () => {
       const response = await axios.get(`https://api.github.com/repos/${repo.full_name}/branches`, {
         headers: {
@@ -130,11 +240,11 @@ const Repo = ({ repo, services, setValue, token }) => {
       });
       return response.data;
     },
-    enabled: !!selected
+    enabled: !!selected && repos?.length > 0
   });
-  //get data from the branch
+
   useQuery({
-    queryKey: ["packageJson", repo.full_name],
+    queryKey: ["packageJson", repo?.full_name],
     queryFn: async () => {
       const response = await axios.get(`https://api.github.com/repos/${repo.full_name}/contents/package.json`, {
         headers: {
@@ -143,7 +253,7 @@ const Repo = ({ repo, services, setValue, token }) => {
       });
       return response.data;
     },
-    enabled: !!selected,
+    enabled: !!selected && repos?.length > 0,
     onSettled: data => {
       if (data?.content === undefined) return;
       const content = atob(data.content);
@@ -155,62 +265,47 @@ const Repo = ({ repo, services, setValue, token }) => {
   console.log(packageJson);
 
   return (
-    <div className="flex w-full flex-col gap-6 px-8 py-3">
-      <div className="flex w-full items-center justify-between gap-3">
-        <p>{repo.name}</p>
-        <Button
-          variant={selected ? "outline" : "default"}
-          onClick={() => {
-            setValue("services.0.env", [
-              { id: nanoid(), key: "REPO_URL", value: repo.html_url, isSecret: false },
-              { id: nanoid(), key: "BRANCH_NAME", value: "main", isSecret: false },
-              { id: nanoid(), key: "ACCESS_TOKEN", value: token, isSecret: true }
-            ]);
-          }}
-        >
-          {selected ? "Selected" : "Select"}
-        </Button>
+    <div className="flex flex-col gap-5 rounded border bg-card px-6 py-6 text-card-foreground">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-semibold">Select Branch</h1>
+        <p className="text-muted-foreground">Select a branch to use for deployment</p>
       </div>
-      {selected && (
-        <div className="mb-4 flex w-full flex-col gap-6 rounded-lg border p-4">
-          <div className="flex w-full items-center justify-between gap-3">
-            <p>Choose Branch</p>
-            {branchesLoading ? (
-              <Spinner size="small" />
-            ) : (
-              <Select
-                onValueChange={value => {
-                  setValue("services.0.env", [
-                    { id: nanoid(), key: "REPO_URL", value: repo.html_url, isSecret: false },
-                    { id: nanoid(), key: "BRANCH_NAME", value: value, isSecret: false },
-                    { id: nanoid(), key: "ACCESS_TOKEN", value: token, isSecret: true }
-                  ]);
-                }}
-              >
-                <SelectTrigger className="ml-1 w-[10rem]">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    {branches?.map((branch: any) => (
-                      <SelectItem key={branch.name} value={branch.name}>
-                        {branch.name}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            )}
+
+      <Select
+        disabled={!selected}
+        onValueChange={value => {
+          setValue("services.0.env", [
+            { id: nanoid(), key: "REPO_URL", value: repo.html_url, isSecret: false },
+            { id: nanoid(), key: "BRANCH_NAME", value: value, isSecret: false },
+            { id: nanoid(), key: "ACCESS_TOKEN", value: token, isSecret: true }
+          ]);
+        }}
+      >
+        <SelectTrigger className="w-full">
+          <div className="flex items-center gap-2">
+            {branchesLoading && <Spinner size="small" />}
+            <SelectValue placeholder="Select" />
           </div>
-          {/* name the framework and version of the package.json file from base64 content */}
-          {packageJson && (
-            <div className="flex w-full items-center justify-between gap-3">
-              <p>Framework</p>
-              <p className="capitalize">{packageJson?.scripts?.start?.split(" ")[0]}</p>
-            </div>
-          )}
-        </div>
-      )}
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {branches?.map((branch: any) => (
+              <SelectItem key={branch.name} value={branch.name}>
+                {branch.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
   );
 };
+{
+  /*         
+{packageJson && (
+  <div className="flex w-full items-center justify-between gap-3">
+    <p>Framework</p>
+    <p className="capitalize">{packageJson?.scripts?.start?.split(" ")[0]}</p>
+  </div>
+)} */
+}
