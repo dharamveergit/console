d650a8692bcfb981b3d18e611469160a1e8ec231
dharamveergit <dharamveersingh.em@gmail.com>
2024-07-20 13:51:54 +0530

    chore: Update isDev variable to always be true in next.config.js

--- apps/deploy-web/next.config.js
+++ apps/deploy-web/next.config.js
@@ -2,7 +2,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
 const { version } = require("./package.json");
-const isDev = process.env.NODE_ENV === "development";
+const isDev = true;
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: isDev
@@ -51,7 +51,7 @@ const moduleExports = {
     config.externals.push("pino-pretty");
     return config;
   },
-  rewrites: [{ source: "/(.*)", destination: "/" }],
+
   redirects: async () => {
     return [
       {
